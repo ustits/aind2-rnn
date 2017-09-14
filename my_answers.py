65c3214cd642c9751d0f0a3efbbc3fcbf9a00a1d
Ruslan Ustits <ustitsruslan@gmail.com>
2017-09-14 23:27:25 +0300

    added implementation for the first task

--- my_answers.py
+++ my_answers.py
@@ -4,16 +4,17 @@ from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import LSTM
 import keras
+import re
 
 
-# TODO: fill out the function below that transforms the input series 
-# and window-size into a set of input/output pairs for use with our RNN model
 def window_transform_series(series, window_size):
     # containers for input/output pairs
     X = []
-    y = []
+    for i in range(0, len(series) - window_size):
+        X.append(series[i:i + window_size])
+    y = series[window_size:]
 
-    # reshape each 
+    # reshape each
     X = np.asarray(X)
     X.shape = (np.shape(X)[0:2])
     y = np.asarray(y)
@@ -21,14 +22,21 @@ def window_transform_series(series, window_size):
 
     return X,y
 
-# TODO: build an RNN to perform regression on our time series input/output data
 def build_part1_RNN(window_size):
-    pass
+    model = Sequential()
+    model.add(LSTM(5, input_shape=(window_size, 1)))
+    model.add(Dense(1))
+    return model
 
 
-### TODO: return the text input with only ascii lowercase and the punctuation given below included.
 def cleaned_text(text):
-    punctuation = ['!', ',', '.', ':', ';', '?']
+    punctuation = ['!', ',', '.', ':', ';', '?', '"', '\'', '(', ')', '&', '%']
+    for symbol in punctuation:
+        text = text.replace(symbol, '')
+
+    abbreviations = [r'\betc\b', r'\bDr\b', r'\bCo\b', r'\B-\B']
+    for abbr in abbreviations:
+        text = re.sub(abbr, '', text)
 
     return text
 
